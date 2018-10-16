b3619e7de37eeae63b233bd3f0b831dad8fb85be
Steven Bruno <sbruno636@gmail.com>
2018-10-16 14:45:35 -0500

    plan functions to be completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let container = document.querySelector('#grid_container');
 
-
+// modify #grid_container grid style so that 16, 2.2 becomes numRows, 35.2/2.2vw
 function createGrid(numRows) {
     for (let i = 0; i < numRows; i++) {
         for (let j = 0; j < numRows; j++) {
@@ -9,6 +9,24 @@ function createGrid(numRows) {
             container.appendChild(box);
         }
     }
+}
+
+
+// create a variable color and reassign its value when one of the buttons is selected, 
+// consider scope, set initial color to black?
+function colorSelect() {
+    //
+}
+
+
+let grid_items = document.querySelectorAll(".grid_item");
+// add event listeners (mousedown?) to grid items and change their class
+// to the selected color
+
+
+function resetBoard() {
+    //execute when the reset board button is clicked
+    //set color of all grid_item elements to starting color
 }
 
 
